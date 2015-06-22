79ed6f37003f079e6886f299e77aa4dac014891c
John McCormack <mccormack.wgsi@gmail.com>
2015-06-22 16:32:40 -0400

    Added in functionality that creates 10 points confined to the room upon clicking the button

--- Assets/Scripts/buttonPress.cs
+++ Assets/Scripts/buttonPress.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class buttonPress : MonoBehaviour {
 
 	public GameObject LIDAR;
+	public GameObject collectable;
 	public GameObject[] pointArray = new GameObject[370];
 
 	// Use this for initialization
@@ -15,6 +16,24 @@ public class buttonPress : MonoBehaviour {
 				point.GetComponent<pointNormal>().saveNormal(length);
 			} catch {
 
+			}
+		}
+		//Generate 10 random point
+		for(int i = 0; i < 10; i++){
+			int randAngle = Random.Range (0, 360);
+			float magnitude = pointArray[randAngle].transform.position.magnitude;
+			if (magnitude == 0){
+				i--;
+			} else {
+				float randomMagnitude = Random.Range (0.0f, (magnitude * 0.9f));
+
+				float radians = randAngle * (Mathf.PI / 180);
+				float x = randomMagnitude * Mathf.Sin (radians);
+				float y = randomMagnitude * Mathf.Cos (radians);
+				float z = 0.0f;
+
+				Vector3 locationVector3 = new Vector3 (x, y, z);
+				GameObject collect = (GameObject)Instantiate (collectable, locationVector3, Quaternion.identity);
 			}
 		}
 	}
