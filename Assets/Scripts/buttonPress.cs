61ac074544f4faef3aee61d3b5a42930773e3438
John McCormack <mccormack.wgsi@gmail.com>
2015-06-24 18:30:33 -0400

    Adding in postgres Database connections

--- Assets/Scripts/buttonPress.cs
+++ Assets/Scripts/buttonPress.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Npgsql;
+
 
 public class buttonPress : MonoBehaviour {
 
+	public NpgsqlConnection conn;
 	public GameObject LIDAR;
 	public GameObject collectable;
 	public GameObject[] pointArray = new GameObject[370];
@@ -10,6 +13,11 @@ public class buttonPress : MonoBehaviour {
 	// Use this for initialization
 	public void press () {
 		pointArray = LIDAR.GetComponent<UDPTest>().pointArray;
+		PostGresUtility();
+		print("Success!");
+		NpgsqlCommand command = conn.CreateCommand();
+
+		/*
 		foreach(GameObject point in pointArray){
 			try{
 				float length = point.transform.position.magnitude;
@@ -36,5 +44,13 @@ public class buttonPress : MonoBehaviour {
 				GameObject collect = (GameObject)Instantiate (collectable, locationVector3, Quaternion.identity);
 			}
 		}
+		*/
+	}
+
+	public void PostGresUtility(string server = "127.0.0.1", string port = "5432", string user_id = "postgres", string password = "password", string database = "postgres"){
+		string connectionString = "Server=" + server + ";Port=" + port + ";User Id=" + user_id + ";Password=" + password + ";Database=" + database;
+		//conn = new NpgsqlConnection ("Server=localhost;Port=5432;User Id=postgres;Password=password;Database=postgres");
+		conn = new NpgsqlConnection (connectionString);
+		conn.Open();
 	}
 }
